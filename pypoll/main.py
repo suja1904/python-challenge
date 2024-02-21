1ab9f92a719b041662e98f5ed3835c91d750ea85
Suja Parekh <157570695+suja1904@users.noreply.github.com>
2024-02-21 12:58:18 -0800

    Update main.py

--- pypoll/main.py
+++ pypoll/main.py
@@ -1,1 +1,23 @@
+import os
+import csv
+
+#  Path to collect data from the Resources fold
+election_csv = os.path.join('.', 'election_data.csv')
+
+# Read in the CSV file
+with open(election_csv, 'r') as csvfile:
+
+    # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    header = next(csvreader)
+    
+    # Loop through the data
+    for row in csvreader:
+        
+        count = count + 1
+        
+           
+
+print(count)
 
