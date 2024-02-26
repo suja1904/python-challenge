e3cb0c317a71a7c9d0c2d4167b6a277fd6f15fa8
Suja Parekh <sujaparekh@Sujas-MacBook-Pro.local>
2024-02-25 16:51:30 -0800

    format print results and add results file

--- pybank/main.py
+++ pybank/main.py
@@ -2,7 +2,7 @@ import os
 import csv
 
 #  Path to collect data from the Resources fold
-budget_csv = os.path.join('.', 'budget_data.csv')
+budget_csv = os.path.join('.', 'pybank', 'Resources', 'budget_data.csv')
 
 # Read in the CSV file
 with open(budget_csv, 'r') as csvfile:
@@ -42,10 +42,11 @@ with open(budget_csv, 'r') as csvfile:
             
     average = totalchange/(count-1)        
 
-print(count)
-print(total)
-print(giamount)
-print(gidate)
-print(gdamount)
-print(gddate)
-print(average)
+print ("Financial Analysis")
+print("----------------------------")
+print("Total Months: ", count)
+print("Total: $" + str(total))
+print("Average Change: $" + "%.2f" % average)
+print("Greatest Increase in Profits: " + str(gidate) + " $" + str(giamount))
+print("Greatest Decrease in Profits: " + str(gddate) + " $" + str(gdamount))
+
